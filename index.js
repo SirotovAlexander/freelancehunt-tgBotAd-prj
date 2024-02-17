ffc501190ef958c52e3f61c1bf64f193aab5fdc6
SirotovAlexander <alexander.sirotov@gmail.com>
2024-02-17 03:36:57 +0200

    up
    up

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
-const TelegramBot = require("node-telegram-bot-api");
-const axios = require("axios");
+// const TelegramBot = require("node-telegram-bot-api");
+// const axios = require("axios");
 require("dotenv").config();
 
-const bot = new TelegramBot(process.env.API_KEY_BOT, {
-  polling: {
-    interval: 2000,
-    autoStart: true,
+// const bot = new TelegramBot(process.env.API_KEY_BOT, {
+//   polling: {
+//     interval: 2000,
+//     autoStart: true,
+//   },
+// });
+
+// bot.on("text", async (msg) => {
+//   console.log(msg);
+//   await bot.sendMessage(msg.chat.id, msg.text);
+// });
+const path = require("path");
+const MTProto = require("@mtproto/core");
+new MTProto({
+  storageOptions: {
+    path: path.resolve(__dirname, "./data/1.json"),
   },
 });
 
-// console.log(process.env.API_KEY_BOT);
-// bot.on("polling_error", (err) => console.log(err.data.error.message));
-bot.on("text", async (msg) => {
-  console.log(msg);
-  await bot.sendMessage(msg.chat.id, msg.text);
-  await axios
-    .post("https://t.me/sadasdaqw", {
-      message_id: 35,
-      from: {
-        id: 6110004577,
-        is_bot: false,
-        first_name: "467bb057db35b17f58d663158d63a0b2",
-        username: "two_face_project",
-        language_code: "ru",
-      },
-      chat: {
-        id: 6110004577,
-        first_name: "467bb057db35b17f58d663158d63a0b2",
-        username: "two_face_project",
-        type: "private",
-      },
-      date: 1708124209,
-      text: `${msg.text}`,
-    })
-    .then((res) => {
-      console.log("успешно отправлено");
-    })
-    .catch((err) => console.log(err.massege));
+const API_ID = process.env.API_ID;
+const API_HASH = process.env.API_HASH;
+
+// 1. Create instance
+const mtproto = new MTProto({
+  API_ID,
+  API_HASH,
+  storageOptions: {
+    path: path.resolve(__dirname, "./data/1.json"),
+  },
+});
+
+// 2. Print the user country code
+mtproto.call("help.getNearestDc").then((result) => {
+  console.log("country:", result.country).catch((err) => console.error(err));
 });
