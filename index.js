173807ee712ffe1de5533c40d1017bed77a8f261
SirotovAlexander <alexander.sirotov@gmail.com>
2024-02-19 00:31:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const TelegramBot = require("node-telegram-bot-api");
-const { Api, TelegramClient } = require("telegram");
-const { StringSession } = require("telegram/sessions");
+// const { Api, TelegramClient } = require("telegram");
+// const { StringSession } = require("telegram/sessions");
 
 require("dotenv").config();
 
-const apiId = 21198973;
-const session = new StringSession(process.env.STRING_SESSION); // You should put your string session here
-const client = new TelegramClient(session, apiId, process.env.API_HASH, {});
+// const apiId = 21198973;
+// const session = new StringSession(process.env.STRING_SESSION); // You should put your string session here
+// const client = new TelegramClient(session, apiId, process.env.API_HASH, {});
 
 const bot = new TelegramBot(process.env.API_KEY_BOT, {
   polling: {
@@ -18,23 +18,34 @@ const bot = new TelegramBot(process.env.API_KEY_BOT, {
 bot.on("text", async (msg) => {
   console.log(msg);
   await bot.sendMessage(msg.chat.id, msg.text);
-  (async function run() {
-    await client.connect(); // This assumes you have already authenticated with .start()
-
-    const result = await client.invoke(
-      new Api.messages.SendMessage({
-        // peer: "two_face_project",
-        noWebpage: true,
-        background: true,
-        clearDraft: true,
-        peer: "https://t.me/mariia2208",
-        replyToMsgId: 6872073,
-        message: msg.text,
-        randomId: Math.floor(Math.random() * 1000000),
-        scheduleDate: 789917,
-      })
-    );
-    console.log(result); // prints the result
-    // process.exit();
-  })();
 });
+
+////// =====?
+////// =====?
+////// =====?
+////// =====?
+////// =====?
+////// =====?
+////// =====?
+////// =====?
+////// =====?
+///// Func for send messages to group /////
+// (async function run() {
+//   await client.connect(); // This assumes you have already authenticated with .start()
+
+//   const result = await client.invoke(
+//     new Api.messages.SendMessage({
+//       // peer: "two_face_project",
+//       noWebpage: true,
+//       background: true,
+//       clearDraft: true,
+//       peer: "https://t.me/mariia2208",
+//       replyToMsgId: 6872073,
+//       message: msg.text,
+//       randomId: Math.floor(Math.random() * 1000000),
+//       scheduleDate: 789917,
+//     })
+//   );
+//   console.log(result); // prints the result
+//   // process.exit();
+// })();
