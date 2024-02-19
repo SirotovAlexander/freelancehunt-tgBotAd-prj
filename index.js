9aa1b3dce36d8e1d8bd9f8adc2a921022ba8921a
SirotovAlexander <alexander.sirotov@gmail.com>
2024-02-19 03:50:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const TelegramBot = require("node-telegram-bot-api");
-// const { Api, TelegramClient } = require("telegram");
-// const { StringSession } = require("telegram/sessions");
+const { Api, TelegramClient } = require("telegram");
+const { StringSession } = require("telegram/sessions");
 
 require("dotenv").config();
 
-// const apiId = 21198973;
-// const session = new StringSession(process.env.STRING_SESSION); // You should put your string session here
-// const client = new TelegramClient(session, apiId, process.env.API_HASH, {});
+const apiId = 21198973;
+const session = new StringSession(process.env.STRING_SESSION); // You should put your string session here
+const client = new TelegramClient(session, apiId, process.env.API_HASH, {});
 
 const bot = new TelegramBot(process.env.API_KEY_BOT, {
   polling: {
@@ -15,9 +15,100 @@ const bot = new TelegramBot(process.env.API_KEY_BOT, {
   },
 });
 
+let adMessage = null;
+let chanelList = [];
+
+const commands = [
+  {
+    command: "start",
+    description: "Запуск бота",
+  },
+  {
+    command: "msggen",
+    description: "Генерация рекламного сообщения",
+  },
+  {
+    command: "chanelsgen",
+    description: "Генерация массива каналов",
+  },
+  {
+    command: "listcheck",
+    description: "Вывести список каналов для рассылки и текст рекламы",
+  },
+  {
+    command: "flood",
+    description: "Запуск рассылки",
+  },
+];
+
+bot.setMyCommands(commands);
+
 bot.on("text", async (msg) => {
-  console.log(msg);
-  await bot.sendMessage(msg.chat.id, msg.text);
+  try {
+    if (msg.text.startsWith("/start")) {
+      await bot.sendMessage(msg.chat.id, `Hy Vita!`);
+    } else if (msg.text == "/msggen") {
+      await bot.sendMessage(msg.chat.id, `Введи рекламное сообщение.`);
+
+      bot.on("text", async (msg) => {
+        if (msg.text.startsWith("$")) {
+          adMessage = msg.text.slice(1, msg.text.length);
+          await bot.sendMessage(msg.chat.id, `Рекламное сообщение Сохранено!`);
+        }
+      });
+    } else if (msg.text == "/chanelsgen") {
+      await bot.sendMessage(msg.chat.id, `Введи список каналов.`);
+      bot.on("text", async (msg) => {
+        if (msg.text.startsWith("http")) {
+          const strToArr = msg.text.split(",");
+          const newArr = strToArr.map((item) => item.trim());
+          chanelList.push(...newArr);
+          await bot.sendMessage(msg.chat.id, `Cписок каналов Сохранен!`);
+        }
+      });
+    } else if (msg.text == "/listcheck") {
+      await bot.sendMessage(
+        msg.chat.id,
+        `<b>${adMessage}</b>\n<b>${chanelList}</b>`,
+        {
+          parse_mode: "HTML",
+        }
+      );
+      console.log(chanelList);
+    } else if (msg.text == "/flood") {
+      if (adMessage === null) {
+        await bot.sendMessage(msg.chat.id, "Созайте рекламное сообщение!");
+      }
+      if (chanelList[0] === undefined) {
+        await bot.sendMessage(
+          msg.chat.id,
+          "Заполните список каналов для рассылки!"
+        );
+      }
+
+      (async function run() {
+        await client.connect(); // This assumes you have already authenticated with .start()
+
+        const result = await client.invoke(
+          new Api.messages.SendMessage({
+            // peer: "two_face_project",
+            noWebpage: true,
+            background: true,
+            clearDraft: true,
+            peer: `${chanelList}`,
+            replyToMsgId: 6872073,
+            message: `${adMessage}`,
+            randomId: Math.floor(Math.random() * 1000000),
+            scheduleDate: 789917,
+          })
+        );
+        console.log(result); // prints the result
+        // process.exit();
+      })();
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 ////// =====?
