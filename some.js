9e2a00786c4b716aeffd1da2cd832f6a5e859928
SirotovAlexander <alexander.sirotov@gmail.com>
2024-02-26 23:37:06 +0200

    Update some.js

--- some.js
+++ some.js
@@ -1,24 +1,32 @@
-const { TelegramClient } = require("telegram");
+const TelegramBot = require("node-telegram-bot-api");
+const { Api, TelegramClient } = require("telegram");
 const { StringSession } = require("telegram/sessions");
-const input = require("input");
 
-const apiId = 21198973;
-const apiHash = "129fb6187b256a6b35e139c35b19e615";
-const stringSession = new StringSession(""); // fill this later with the value from session.save()
+require("dotenv").config();
 
-(async () => {
-  console.log("Loading interactive example...");
-  const client = new TelegramClient(stringSession, apiId, apiHash, {
-    connectionRetries: 5,
-  });
-  await client.start({
-    phoneNumber: async () => await input.text("Please enter your number: "),
-    password: async () => await input.text("Please enter your password: "),
-    phoneCode: async () =>
-      await input.text("Please enter the code you received: "),
-    onError: (err) => console.log(err),
-  });
-  console.log("You should now be connected.");
-  console.log(client.session.save()); // Save this string to avoid logging in again
-  await client.sendMessage("me", { message: "Hello!" });
-})();
+const apiId = 21854456;
+const session = new StringSession(process.env.STRING_SESSION); // You should put your string session here
+const client = new TelegramClient(session, apiId, process.env.API_HASH, {});
+
+// (async () => {
+//   console.log("Loading interactive example...");
+//   const client = new TelegramClient(stringSession, apiId, apiHash, {
+//     connectionRetries: 5,
+//   });
+//   await client.start({
+//     phoneNumber: async () => await input.text("Please enter your number: "),
+//     password: async () => await input.text("Please enter your password: "),
+//     phoneCode: async () =>
+//       await input.text("Please enter the code you received: "),
+//     onError: (err) => console.log(err),
+//   });
+//   console.log("You should now be connected.");
+//   console.log(client.session.save()); // Save this string to avoid logging in again
+//   await client.sendMessage("me", { message: "Hello!" });
+// })();
+// (async function run() {
+//   await client.connect(); // This assumes you have already authenticated with .start()
+
+//   const result = await client.invoke(new Api.contacts.GetSaved({}));
+//   console.log(result); // prints the result
+// })();
